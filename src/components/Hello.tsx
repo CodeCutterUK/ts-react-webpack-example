50fe1f7b0d3242968201fdc650ac8687bbc8a385
David Bohunek <david.bohunek@sky.uk>
2016-06-20 18:57:29 +0100

    react-fetch added

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import * as Fetch from "react-fetch";
 
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <div>
+                 <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+                 <Fetch url="https://api.github.com/orgs/sky-uk" />
+               </div>;
     }
 }
