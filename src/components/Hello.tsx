684f0f61f9e271a119f869bd4e8c1f2708b2f669
David Bohunek <david.bohunek@sky.uk>
2016-06-20 19:06:12 +0100

    Revert "react-fetch added"
    This reverts commit 50fe1f7b0d3242968201fdc650ac8687bbc8a385.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
-import * as Fetch from "react-fetch";
 
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <div>
-                 <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
-                 <Fetch url="https://api.github.com/orgs/sky-uk" />
-               </div>;
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
